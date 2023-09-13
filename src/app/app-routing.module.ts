ff80cc8dfb3264b03551d47294247ea3e85b36e1
marilynmeza <127436027+marilynmeza@users.noreply.github.com>
2023-09-13 19:42:22 -0300

    primera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+ 
   {
     path: 'ingreso-de-usuario',
     loadChildren: () => import('./ingreso-de-usuario/ingreso-de-usuario.module').then( m => m.IngresoDeUsuarioPageModule)
